4cb31517494426ddd39382fc11ddb887e3248488
lxm <289096620@qq.com>
2020-02-25 19:29:11 +0800

    淹没分析 淹没范围分析

--- src/components/threeDim.vue
+++ src/components/threeDim.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="cesiumContainer">
+    <div id="cesiumContainer" @mousemove="onCesiumMouseMove($event)">
         <div class="location-bar">
             <button @click="onFloodAnalyBtnClick">淹没分析</button>
             <br/>
@@ -19,6 +19,9 @@
             <input type="radio" name="type_map" v-on:click="changeType" v-bind:checked="!map_type">
             <label>深度图</label>
         </div>
+        <div id="div_location_jwd" class="location-bar no-print" style="left: 2px; bottom: 2px;color: white;">
+            {{coordShow}}
+        </div>
     </div>
 </template>
 
@@ -33,6 +36,11 @@
         name: "threeDim",
         data(){
             return{
+                bDraw:true,
+                polygonPts:[],
+                entity:null,
+                coordShow:'',
+                /*手动自动分隔符*/
                 bShowFloodAnaly:false,
                 linePositionList:[],
                 handler:null,
@@ -45,7 +53,10 @@
                     115.9473, 40.0381,
                     115.9614, 40.0073,
                     115.9042, 39.9912
-                ]
+                ],
+                timer:null,
+                speed: 20,
+                map_type:true
             }
         },
         mounted:function(){
@@ -82,9 +93,21 @@
                 };
                 g_viewer = new Cesium.Viewer('cesiumContainer', viewerOption);
 
+                g_viewer.sceneModePicker.viewModel.duration = 0.0;
+
                 let rectangle = new Cesium.Rectangle(Cesium.Math.toRadians(113.59863),Cesium.Math.toRadians(34.738766),Cesium.Math.toRadians(113.88431),Cesium.Math.toRadians(34.89258));
 
                 g_viewer.scene.camera.flyTo({destination: rectangle});
+
+                g_viewer.screenSpaceEventHandler.setInputAction(
+                    this.onCesiumLeftClick,
+                    Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+                g_viewer.screenSpaceEventHandler.setInputAction(
+                    this.onCesiumLeftDoubleClick,
+                    Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
+                g_viewer._cesiumWidget._creditContainer.style.display = "none";// 隐藏版权
             },
             onFloodAnalyBtnClick:function () {
                 this.bShowFloodAnaly = !this.bShowFloodAnaly;
@@ -92,6 +115,15 @@
             start:function () {
                 this.initViewStatus();
                 this.addDisListener();
+
+                this.timer = window.setInterval(() => {
+                    if ((this.height_max > this.extrudedHeight) && (this.extrudedHeight >= this.height_min)) {
+                        this.extrudedHeight = this.extrudedHeight + this.speed
+                    } else {
+                        this.extrudedHeight = this.height_min
+                    }
+                }, 500);
+                this.drawPoly(this.polygon_degrees)
             },
             initViewStatus:function () {
                 let scene = g_viewer.scene;
@@ -115,7 +147,7 @@
 
                 // 绘制线
                 this.drawLine(linePositionList);
-                //this.loadGrandCanyon();
+                this.loadGrandCanyon();
 
                 this.drawPoly(this.polygon_degrees);
             },
@@ -178,7 +210,94 @@
                 this.polygonEntities.push(entity);
             },
             onBtnDraw:function () {
-                
+                //g_viewer.scene.morphTo2D(0);
+                this.initViewStatus();
+                g_viewer.entities.removeAll();
+                this.entity = null;
+                this.bDraw = true;
+                this.polygonPts = [];
+            },
+            onCesiumLeftClick:function (movement) {
+                //屏幕坐标
+                var wp = g_viewer.scene.globe.pick(
+                    g_viewer.camera.getPickRay(movement.position),
+                    g_viewer.scene);
+                var screenPt = new Cesium.Cartesian2(movement.position.x, movement.position.y);
+                //世界坐标
+                var carPt = g_viewer.scene.globe.pick(g_viewer.camera.getPickRay(screenPt), g_viewer.scene);
+                var catG = g_viewer.scene.globe.ellipsoid.cartesianToCartographic(carPt);
+                var xx = Cesium.Math.toDegrees(catG.longitude);
+                var yy = Cesium.Math.toDegrees(catG.latitude);
+                var hh = catG.height;
+
+                if(this.bDraw){
+                    if(this.entity == null){
+                        //this.polygonPts.push(xx,yy,hh);
+                        this.polygonPts.push(xx,yy);
+
+                        this.entity = g_viewer.entities.add({
+                            polygon:{
+                                hierarchy : new Cesium.CallbackProperty(this.updateHierarchy, false),//Cesium.Cartesian3.fromDegreesArray(this.polygonPts),
+                                material: new Cesium.Color.fromBytes(64, 157, 253, 100),
+                                perPositionHeight: true,
+                                extrudedHeight:250
+                            }
+                        });
+                        //this.entity.polygon.hierarchy._value.positions = new Cesium.CallbackProperty(this.updateHierarchy, false);
+                    }
+                    else{
+                        this.polygonPts.push(xx,yy);
+                        if((this.polygonPts.length / 2) == 2){
+                            this.polygonPts.push(xx,yy);
+                        }
+                    }
+                }
+            },
+            updateHierarchy:function(time, result){
+                return new Cesium.PolygonHierarchy(
+                    //Cesium.Cartesian3.fromDegreesArrayHeights(this.polygonPts),
+                    Cesium.Cartesian3.fromDegreesArray(this.polygonPts),
+                    []);
+            },
+            onCesiumLeftDoubleClick:function(movement){
+                this.bDraw = false;
+                //g_viewer.scene.morphTo3D(0);
+            },
+            onCesiumMouseMove:function (e) {
+                //屏幕坐标
+                let screenPt = new Cesium.Cartesian2(e.clientX, e.clientY);
+
+                //世界坐标
+                let carPt = g_viewer.scene.globe.pick(g_viewer.camera.getPickRay(screenPt), g_viewer.scene);
+                let catG = g_viewer.scene.globe.ellipsoid.cartesianToCartographic(carPt);
+                let xx = Cesium.Math.toDegrees(catG.longitude);
+                let yy = Cesium.Math.toDegrees(catG.latitude);
+                let hh = catG.height;
+
+                if(this.bDraw){
+                    if((this.polygonPts.length / 2)  >= 3){
+                        /*this.polygonPts[this.polygonPts.length - 3] = xx;
+                        this.polygonPts[this.polygonPts.length - 2] = yy;
+                        this.polygonPts[this.polygonPts.length - 1] = hh;*/
+                        this.polygonPts[this.polygonPts.length - 2] = xx;
+                        this.polygonPts[this.polygonPts.length - 1] = yy;
+                    }
+                }
+
+                this.coordShow = "经度:" + xx + "  纬度:" + yy + "  高程:" + parseInt(hh) + "米"
+            },
+            clear:function () {
+                if(this.timer){
+                    window.clearInterval(this.timer);
+                    this.timer = null;
+                }
+                this.extrudedHeight = this.height_min;
+
+                for (let eTmp of this.polygonEntities) {
+                    g_viewer.entities.remove(eTmp)
+                }
+                g_viewer.skyAtmosphere = true
+                g_viewer.scene.globe.clippingPlanes = null;
             }
         }
     }
